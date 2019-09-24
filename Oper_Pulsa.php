81659aae66fb703ad0f59f3424dcb5fd7feaa87e
Adrian Abdillah <53867081+xadrian57@users.noreply.github.com>
2019-09-24 10:41:30 +0700

    Update and rename Oper_sosmed.php to Oper_Pulsa.php

--- Oper_Pulsa.php
+++ Oper_Pulsa.php
@@ -1,14 +1,13 @@
 // api data
             $service_id = 123; //id service
             $api_key = "api_key";
-            $target = "adrian_abdllh";
-             $jumlah = 100;
+            $target = "080000000";
             // end api data
 //SCRIPT AUTO OPER(ORDER SOSMED) NESPEDIA BY ADRIAN57
            
-                $api_postdata = "key=$api_key&action=order&service=$service_id&target=$target&quantity=$jumlah";
+                $api_postdata = "api_key=$api_key&action=pemesanan&layanan=$service_id&target=$target";
                 $ch = curl_init();
-                curl_setopt($ch, CURLOPT_URL, $api_link);
+                curl_setopt($ch, CURLOPT_URL, "https://nespedia-panel.com/api/pulsa");
                 curl_setopt($ch, CURLOPT_POST, 1);
                 curl_setopt($ch, CURLOPT_POSTFIELDS, $api_postdata);
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -16,8 +15,8 @@
                 $chresult = curl_exec($ch);
                 curl_close($ch);
                 $json_result = json_decode($chresult);
-                 if ($json_result['error'] == true) {
-    echo "ORDER GAGAL, PESAN : ".$json_result['error'];
-} else {
+                 if ($json_result['status'] == true) {
     echo "ORDER SUKSES, ORDER ID : ".$json_result['data']['id'];
+} else {
+    echo "ORDER GAGAL, PESAN : ".$json_result['data']['pesan'];
 }
