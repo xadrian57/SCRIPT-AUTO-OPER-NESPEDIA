c8f0dbd79b72206ddd54a465659430369d428ad1
Adrian Abdillah <53867081+xadrian57@users.noreply.github.com>
2019-10-18 22:56:53 +0700

    Update Get_LayananPulsa.php

--- Get_LayananPulsa.php
+++ Get_LayananPulsa.php
@@ -1,7 +1,20 @@
+<?php
+//Get Service(layanan) Pulsa Nespedia By Adrian57
+
+
+
+//api data
 $api_key = "api_key_Lo";
-$postdata = "api_key=$api_key&action=layanan";
- 
-//SCRIPT AUTO OPER(SERVICE PULSA) NESPEDIA BY ADRIAN
+//end api data
+
+//postdata
+$postdata = array('api_key' => $api_key,
+                  'action'  => 'layanan'
+                  );
+//end postdata
+
+
+//Curl
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "https://nespedia-panel.com/api/pulsa");
 curl_setopt($ch, CURLOPT_POST, 1);
@@ -11,9 +24,12 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 $chresult = curl_exec($ch);
 curl_close($ch);
 $json_result = json_decode($chresult, true);
+//end curl
+
 $indeks=0;
 $i = 1;
 // get data service
+
 while($indeks < count($json_result['data'])){
    
 $sid=$json_result['data'][$indeks]['sid'];
@@ -24,7 +40,10 @@ $status = $json_result['data'][$indeks]['status'];
 $indeks++;
 $i++;
 // end get data service
+
 // setting price
 $rate = $price;
 $rate_asli = $rate + 800; // Masukan Penambahan Harga Sesukamu
 // setting price
+
+?>
