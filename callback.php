586a478aaac4fa5fe6d3fa140c56af23802fbc3b
Adrian Abdillah <53867081+xadrian57@users.noreply.github.com>
2022-12-28 16:18:52 +0700

    Update callback.php

--- callback.php
+++ callback.php
@@ -1,11 +1,11 @@
 <?php
 require("config.php"); //hubungkan kepada file config
  
-Script callback Pulsa By adrian57
+//Script callback Pulsa By adrian57
 
-$ip_nesped = "103.126.226.11"; //Ip server nespedia
+$ip_nesped = "103.147.154.152"; //Ip server nespay
  
-if( $_SERVER['REMOTE_ADDR'] == $ip_nesped ){
+if( $_SERVER['REMOTE_ADDR'] == $ip_nespay ){
    
     $data_masuk = $_POST['content'];
     $json = json_decode($data_masuk,TRUE);
@@ -18,7 +18,7 @@ if( $_SERVER['REMOTE_ADDR'] == $ip_nesped ){
     $date       = $json['date'];
     $catatan    = $json['keterangan'];
    
-    $sql             = "UPDATE pembelian_pulsa SET status = '$status' , keterangan = '$catatan' WHERE provider_oid = '$oid' AND provider = 'NES-PULSA'";// Memyimpan kepada database (edit sesuaikan)
+    $sql             = "UPDATE pembelian_pulsa SET status = '$status' , keterangan = '$catatan' WHERE provider_oid = '$oid' AND provider = 'NES-PAY'";// Memyimpan kepada database (edit sesuaikan)
    
     $ok   = mysqli_query($conn,$sql);
    
